af7ca4e55b5e861ee1be30222d0fe62a529d419e
anjue39 <98279987+anjue39@users.noreply.github.com>
2025-02-15 06:36:50 +0800

    Update clash.py

--- utils/clashcheck/clash.py
+++ utils/clashcheck/clash.py
@@ -141,7 +141,6 @@ def filter(config):
                     try:
                         if 'tls' not in x or not x['tls']:
                             x['tls'] = True
-                            continue
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'GRPC'
                         authentication = 'password'
                     except:
@@ -151,7 +150,6 @@ def filter(config):
                     try:
                         if 'tls' not in x or not x['tls']:
                             x['tls'] = True
-                            continue
                         x['name'] = str(flag.flag(country)) + ' ' + str(country) + ' ' + str(count) + ' ' + 'H2'
                         authentication = 'password'
                     except:
